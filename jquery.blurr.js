c653c233e043490878b14bb6ec260cc7b976f9b5
Tom Hallam <tom@freshnode.co.uk>
2014-07-31 17:58:56 +0100

    Fixed element counting, encapsulated CSS

--- jquery.blurr.js
+++ jquery.blurr.js
@@ -8,11 +8,12 @@
                 offsetX: 0,
                 offsetY: 0, 
                 sharpness: 40,
+                height: 300,
                 callback: function() {}
             };
 
     // The actual plugin constructor
-    function Blurr(element, options) {
+    function Blurr(element, options, elementIndex) {
         this.$el = $(element);
         // jQuery has an extend method which merges the contents of two or
         // more objects, storing the result in the first object. The first object
@@ -26,7 +27,7 @@
         this.tpl = '<svg><defs><filter id="blrIMG{{i}}"><feGaussianBlur id="filter_1" stdDeviation="{{sharpness}}" data-filterid="1"></feGaussianBlur><feComponentTransfer><feFuncR type="linear" slope="0.4"></feFuncR><feFuncG type="linear" slope="0.4"></feFuncG><feFuncB type="linear" slope="0.4"></feFuncB></feComponentTransfer></filter></defs><image x="{{offsetX}}" y="{{offsetY}}" width="100%" height="100%" xlink:href="{{href}}" filter="url(#blrIMG{{i}})" preserveAspectRatio="xMidYMid slice"></image></svg>';
         
         // Element counter
-        this.elementCount = 0;
+        this.elementCount = elementIndex;
         
         // Initialise the plugin
         this.init();
@@ -38,7 +39,7 @@
         init: function() {
                         
             // Import options from the data-attributes of the element
-            var href, offsetX, offsetY, sharpness, callback;
+            var href, offsetX, offsetY, sharpness, callback, height;
             
             // Assign from the options, if available - [data-] attributes override below
             href      = this.settings.href;
@@ -46,6 +47,7 @@
             offsetY   = this.settings.offsetY;
             sharpness = this.settings.sharpness;
             callback  = this.settings.callback;
+            height    = this.settings.height;
             
             if(this.$el.data('image')) {
                 href = this.$el.data('image');
@@ -82,25 +84,38 @@
             else {
                 sharpness = 100 - sharpness;
             }
+            
+            if(typeof height === 'undefined' || sharpness.length === 0 || sharpness < 0) {
+                height = 300;
+            }
                         
             // Add the blurstretch CSS class
             this.$el.addClass('has-blurr');
             
             // Parse, render and callback
-            return this.renderSVG(href, offsetX, offsetY, sharpness, callback);
+            return this.renderSVG(href, offsetX, offsetY, sharpness, height, callback);
 
         },
-        renderSVG: function(href, offsetX, offsetY, sharpness, callback) {
+        renderSVG: function(href, offsetX, offsetY, sharpness, height, callback) {
             
-           var _tpl = this.tpl;
+            // Parse the template and replace values
+            var _tpl = this.tpl;
             _tpl = _tpl.replace('{{href}}', href);
             _tpl = _tpl.replace('{{offsetX}}', offsetX);
             _tpl = _tpl.replace('{{offsetY}}', offsetY);
             _tpl = _tpl.replace('{{sharpness}}', sharpness);
             _tpl = _tpl.replace(/{{i}}/g, this.elementCount);
 
+            // Prepend the template to the wrapper
             $(_tpl).appendTo(this.$el);
             
+            // Format the target div
+            this.$el.css({
+                'height': height,
+                'overflow': 'hidden'
+            });
+            
+            // Format the SVG with some tweaks to make it look grand.
             this.$el.find('svg').css({
                 'min-width': '110%',
                 'min-height': '110%',
@@ -111,6 +126,7 @@
                 'left': 0
             });
 
+            // Format the inner div with some styles to make sure it shows
             this.$el.find('div:first').css({
                 'position': 'absolute',
                 'left': 0,
@@ -118,6 +134,7 @@
                 'z-index': 100
             });
             
+            // Call the callback
             if(typeof callback === 'function') {
                 callback.call(this, href, offsetX, offsetY, sharpness);
             }
@@ -128,12 +145,14 @@
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
     $.fn[ pluginName ] = function(options) {
-        this.each(function() {
+        var self = this;
+        this.each(function(i) {
             if (!$.data(this, "plugin_" + pluginName)) {
-                $.data(this, "plugin_" + pluginName, new Blurr(this, options));
+                $.data(this, "plugin_" + pluginName, new Blurr(this, options, i));
             }
-            this.elementCount++;
         });
+
+        console
 
         // chain jQuery functions
         return this;
